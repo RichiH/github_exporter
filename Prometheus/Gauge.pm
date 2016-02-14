6386956bcc91e2cd6e52b265e0b394e698a65311
Richard Hartmann <richih@debian.org>
2016-02-14 16:27:40 +0100

    Prometheus/Gauge.pm: Saner float detection
    I don't check if it's float64, just if the string matches...

--- Prometheus/Gauge.pm
+++ Prometheus/Gauge.pm
@@ -6,6 +6,7 @@ use diagnostics;
 
 use Data::Dumper;
 
+
 # TODO track internal errors
 
 sub new {
@@ -27,7 +28,7 @@ sub add {
 	my %options = @_;
 
 	die "Value '$options{value}' is empty\n" unless defined $options{value};
-	die "Value '$options{value}' is not a float\n" unless $options{value} =~ /^[+-]?(?=\.?\d)\d*\.?\d*(?:e[+-]?\d+)?\z/i;
+	die "Value '$options{value}' is not a float\n" unless $options{value} =~ /^  [+-]? ( (\d+ (\.\d*)?)  |  (\.\d+) ) $/x;
 
 	$self->{text} .= $self->{name}. "{". $options{labels}. "} $options{value}\n";
 }
